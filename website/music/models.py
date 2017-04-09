4f18cf7df683e28ece50ad3c41e14ef77033ffa8
Ross Marshall <rossmarshallgithub@gmail.com>
2017-04-08 23:59:40 -0400

    Chapter 12

--- website/music/models.py
+++ website/music/models.py
@@ -11,8 +11,11 @@ class Album(models.Model):
      genre = models.CharField(max_length=100)
      album_logo = models.CharField(max_length=1000)
 
+     def __str__(self):  ### __ = dunder (double under)
+         return self.album_title + ' - ' + self.artist
 
-class Songs(models.Model):
+
+class Song(models.Model):
      album = models.ForeignKey(Album, on_delete=models.CASCADE)
      file_type = models.CharField(max_length=10)
      song_title = models.CharField(max_length=250)
