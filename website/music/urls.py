81aaf11af38c21c05f707046824dd482dca6fc60
Ross Marshall <rossmarshallgithub@gmail.com>
2017-04-10 09:41:52 -0400

    Chapter 20 - Removing Hardcoded URLs

--- website/music/urls.py
+++ website/music/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),
     
-    # /music/71/   ^ = begin $ = end
+    # /music/71/   ^ = begin $ = end /music/album id
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
 
